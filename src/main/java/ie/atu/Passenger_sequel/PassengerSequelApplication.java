ac61753e7a6a94ab71af6109eaf7fadb30c1b0f8
Shannon3335 <gyRvVB2dzYTEF2a>
2022-11-23 22:23:04 +0000

    added getPassenger

--- src/main/java/ie/atu/Passenger_sequel/PassengerSequelApplication.java
+++ src/main/java/ie/atu/Passenger_sequel/PassengerSequelApplication.java
@@ -2,8 +2,7 @@ package ie.atu.Passenger_sequel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -11,6 +10,7 @@ import java.util.List;
 @RequestMapping(path = "api/passenger")
 public class PassengerSequelApplication {
 
+	@GetMapping(path = "/getPassengers")
 	public List<Passenger> getPassengers()
 	{
 		List<Passenger> myPassengers = List.of(
@@ -19,6 +19,12 @@ public class PassengerSequelApplication {
 				new Passenger("Mr", "Rick",53453242323L, 35442346L, 25)
 		);
 		return myPassengers;
+	}
+
+	@GetMapping(path = "/getPassenger")
+	public Passenger getPassenger(@PathVariable (value = "passengerID") String passengerID) {
+		Passenger p = new Passenger("Mr", "Robb", 2472872342L, 3534324L, 25);
+		return p;
 	}
 
 	public static void main(String[] args) {
